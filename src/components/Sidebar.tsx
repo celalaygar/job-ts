616a94bd512f75f0d9a042e6f9035c425950d14a
hacı celal aygar <aygarcelal@gmail.com>
2025-01-29 02:48:06 +0300

    fix sidebar icons padding

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,13 +2,18 @@ import React, { useState } from 'react';
 import { ListItemIcon, ListItemText } from '@mui/material';
 import { People, Assignment, Timeline, ListAlt, Home, Person } from '@mui/icons-material';
 import Link from 'next/link';
-import sidebarLinks from '../data/sidebarLinks';
+import sidebarLinks, { SidebarLink } from '../data/sidebarLinks';
 import MenuIcon from '@mui/icons-material/Menu';
 const Sidebar = () => {
     const [isCollapsed, setIsCollapsed] = useState(false);
+    const [globalActiveKey, setGlobalActiveKey] = React.useState<string>(sessionStorage.getItem("globalActiveKey") || "home");
 
     const toggleSidebar = () => {
         setIsCollapsed(!isCollapsed);
+    };
+    const setActiveMenu = (activeKey: string) => {
+        setGlobalActiveKey(activeKey)
+        sessionStorage.setItem("globalActiveKey", activeKey)
     };
 
     const icons = {
@@ -29,10 +34,12 @@ const Sidebar = () => {
                 <MenuIcon />
             </button>
             <div className={`h-screen flex flex-col `}>
-                {sidebarLinks.map((link) => (
+                {sidebarLinks.map((link: SidebarLink) => (
                     <Link key={link.label} href={link.href}
+                        onClick={e => setActiveMenu(link.activeKey)}
                         style={{ padding: `${isCollapsed ? ' 10px 10px 11px 10px ' : ' 6px 10px 7px 10px '}` }}
-                        className={'flex items-center  hover:bg-gray-400 rounded '}>
+                        className={`${link.activeKey === sessionStorage.getItem("globalActiveKey") ? ' bg-gray-300 ' : '  '}` +
+                            'flex items-center  hover:bg-gray-200 rounded '}>
                         <ListItemIcon >
                             {icons[link.icon]}
                         </ListItemIcon>
--- src/data/sidebarLinks.ts
+++ src/data/sidebarLinks.ts
@@ -1,12 +1,21 @@
 import links from './links'; // Import links
 
-const sidebarLinks = [
-    { href: links.home, label: 'Anasayfa', icon: 'Home', loginRequired: false },
-    { href: links.profile, label: 'Profilim', icon: 'Person', loginRequired: true },
-    { href: links.users, label: 'Kullanıcılar', icon: 'People', loginRequired: true },
-    { href: links.tasks, label: 'Tasklar', icon: 'Assignment', loginRequired: true },
-    { href: links.sprints, label: 'Sprintler', icon: 'Timeline', loginRequired: true },
-    { href: links.backlog, label: 'Backlog', icon: 'ListAlt', loginRequired: true },
+export interface SidebarLink {
+    href: string;       // Bağlantının URL'si veya rotası
+    label: string;      // Bağlantı için görünen metin
+    icon: string;       // İkonun adı (ör. Material UI, FontAwesome, vb.)
+    loginRequired: boolean; // Bağlantının giriş gerektirip gerektirmediği
+    activeKey: string;  // Aktif durumda kullanılan anahtar
+}
+
+
+const sidebarLinks: SidebarLink[] = [
+    { href: links.home, label: 'Anasayfa', icon: 'Home', loginRequired: false, activeKey: "home" },
+    { href: links.profile, label: 'Profilim', icon: 'Person', loginRequired: true, activeKey: "profile" },
+    { href: links.users, label: 'Kullanıcılar', icon: 'People', loginRequired: true, activeKey: "users" },
+    { href: links.tasks, label: 'Tasklar', icon: 'Assignment', loginRequired: true, activeKey: "tasks" },
+    { href: links.sprints, label: 'Sprintler', icon: 'Timeline', loginRequired: true, activeKey: "sprints" },
+    { href: links.backlog, label: 'Backlog', icon: 'ListAlt', loginRequired: true, activeKey: "backlog" },
 ];
 
 export default sidebarLinks; 
